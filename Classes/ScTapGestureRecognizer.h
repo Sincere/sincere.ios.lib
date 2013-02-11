e80bd37f5d665fd125e88a37c601257b6f373ccd
Masamoto Miyata <miyata@sincere-co.com>
2013-02-11 11:43:58 +0900

    NSDictionaryをパッキングできるTapGestureRecognizer

---
+++ Classes/ScTapGestureRecognizer.h
@@ -0,0 +1,15 @@
+//
+//  ScTapGestureRecognizer.h
+//  fotocase
+//
+//  Created by Masamoto Miyata on 2013/02/08.
+//  Copyright (c) 2013年 Masamoto Miyata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScTapGestureRecognizer : UITapGestureRecognizer
+
+@property (nonatomic, strong) NSDictionary *params;
+
+@end
---
+++ Classes/ScTapGestureRecognizer.m
@@ -0,0 +1,13 @@
+//
+//  ScTapGestureRecognizer.m
+//  fotocase
+//
+//  Created by Masamoto Miyata on 2013/02/08.
+//  Copyright (c) 2013年 Masamoto Miyata. All rights reserved.
+//
+
+#import "ScTapGestureRecognizer.h"
+
+@implementation ScTapGestureRecognizer
+
+@end
