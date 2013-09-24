d1b8fa712fbe444b74b5a6529d8bab7314826709
Masamoto Miyata <miyata@sincere-co.com>
2013-09-24 10:55:20 +0900

    バージョン判別マクロ

---
+++ Classes/ScSystem.h
@@ -0,0 +1,18 @@
+//
+//  ScSystem.h
+//  fotocase
+//
+//  Created by Masamoto Miyata on 2013/09/14.
+//  Copyright (c) 2013年 Masamoto Miyata. All rights reserved.
+//
+
+#ifndef fotocase_ScSystem_h
+#define fotocase_ScSystem_h
+
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+#endif
